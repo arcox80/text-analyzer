e3d57e598b42b0453d69db6dc289423939b684c9
Andrew Cox <drew.r.cox@gmail.com>
2017-02-10 15:40:38 -0500

    Got first two counting functions working

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-// your code here!
+function totalWords(text) {
+  var words = text.split(' ');
+  return words.length;
+}
+
+function totalUnique(text) {
+  var cleanList = text.toLowerCase().split(/[ ,!.";:-]+/).filter(Boolean).sort();
+  var uniqueWords = {};
+  for (var i = 0; i < cleanList.length; i++) {
+        if (uniqueWords.hasOwnProperty(cleanList[i])) {
+            uniqueWords[cleanList[i]]++;
+        } else {
+            uniqueWords[cleanList[i]] = 1;
+        }
+  }
+  var num = Object.keys(uniqueWords).length;
+  return num;
+}
+
+function avgWrdLngth(text) {}
+
+function avgSenLngth(text) {}
+
+function submitButton() {
+  $('form').submit(function(event) {
+    event.preventDefault();
+    var textSubmit = $('textarea').val();
+    var totalResults = totalWords(textSubmit);
+    var uniqueResults = totalUnique(textSubmit);
+    $('dl dd:nth-child(2)').text(totalResults);
+    $('dl dd:nth-child(4)').text(uniqueResults);
+    $('.js-text-report').removeClass('hidden');
+  });
+}
+
+$(submitButton);
